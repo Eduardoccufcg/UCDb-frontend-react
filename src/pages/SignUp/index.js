cf52dcd2ed7eb34586383ef71b09b681b55c8fb4
Eduardoccufcg <eduardo.pereira.santos@ccc.ufcg.edu.br>
2019-12-10 23:34:39 -0300

    Iniciando implementação do componente SignUp

---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from "react";
+
+import {Link} from "react-router-dom";
+
+import Logo from "../../assets/logo.png"
+
---
+++ src/pages/SignUp/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Container = style.div``;
+
+export const Form = style.form``;
