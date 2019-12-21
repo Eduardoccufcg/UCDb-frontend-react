ce0275f3486f515d13d9ba7f15ba5f1661034860
Eduardoccufcg <eduardo.pereira.santos@ccc.ufcg.edu.br>
2019-12-21 12:59:02 -0300

    pronto para o segundo deploy

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Container} from './styles.js'
+import { Container } from './styles.js'
 import { Link, withRouter } from "react-router-dom";
 import { isAuthenticated } from "../../services/auth";
 import Logo from "../../assets/logo.png";
@@ -9,16 +9,21 @@ class Header extends Component {
     render() {
         return (
             <Container>
-                <div id= "imagem-header"><img src={Logo} alt="UCDb logo" /></div>
-                {isAuthenticated() && <div id="menu-header">
-                    <Link to="/top10">TOP 10</Link>
-                    <Link to="/">Sair</Link>
-                </div>}
-                {!isAuthenticated() && <div id="menu-header">
-                    <Link to="/signup">Cadastrar</Link>
-                    <Link to="/">Entrar</Link>
-                </div>}
 
+                <div id="imagem-header"><img src={Logo} alt="UCDb logo" /></div>
+                <div id="menu-header">
+                    <Link to="/app">Inicio</Link>
+                    {isAuthenticated() && <div>
+                        <Link to="/top10">TOP 10</Link>
+                        <Link to="/">Sair</Link>
+                    </div>}
+                    {!isAuthenticated() && <div>
+                        <Link to="/signup">Cadastrar</Link>
+                        <Link to="/">Entrar</Link>
+                    </div>}
+                    
+                </div>
+                
             </Container>
 
         )
