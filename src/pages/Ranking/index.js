6fa16c8cb07294ca49a347406ef62462c849e509
Eduardoccufcg <eduardo.pereira.santos@ccc.ufcg.edu.br>
2020-01-10 11:23:10 -0300

    Listando ranking

--- src/pages/Ranking/index.js
+++ src/pages/Ranking/index.js
@@ -1,35 +1,60 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import Header from '../../components/Header'
-import {Container } from "./styles";
+import { Container } from "./styles";
 import api from "../../services/api";
-import {getToken} from "../../services/auth";
+import { getToken } from "../../services/auth";
 
 
 class Ranking extends Component {
 
     state = {
-        ranking:{}
+        ranking: {}
     }
 
     getPerfis = async () => {
-        await api.get("/api/v1/profiles/ranking",{headers: {'Authorization': "bearer " + getToken}}).then((response) => { this.setState({ ranking: response.data }) })
+        await api.get("/api/v1/profiles/ranking", { headers: { 'Authorization': "bearer " + getToken } }).then((response) => { this.setState({ ranking: response.data }) })
             .catch((error) => {
                 this.setState({ error: error.response.data.message })
             })
-        console.log(this.state.ranking)
+
     }
-    componentDidMount(){
+    componentDidMount() {
 
         this.getPerfis();
 
-    }  
+    }
 
     render() {
 
         return (
 
             <Container>
-               <Header></Header>
+                <Header></Header>
+                <div>
+                    <h1>Ranking de Likes</h1>
+                    {this.state.ranking.rankingLikes !== undefined && this.state.ranking.rankingLikes.map(disciplina => (
+
+                        <article key={disciplina.id}>
+                            <strong>{disciplina.id}</strong>
+                            <p>{disciplina.name}</p>
+                            <p>{disciplina.num}</p>
+                        </article>
+                    ))}
+
+                </div>
+
+                <div>
+                    <h1>Ranking de Comentários</h1>
+                    {this.state.ranking.rankingComments !== undefined && this.state.ranking.rankingComments.map(disciplina => (
+
+                        <article key={disciplina.id}>
+                            <strong>{disciplina.id}</strong>
+                            <p>{disciplina.name}</p>
+                            <p>{disciplina.num}</p>
+                        </article>
+                    ))}
+
+                </div>
             </Container>
         );
 
